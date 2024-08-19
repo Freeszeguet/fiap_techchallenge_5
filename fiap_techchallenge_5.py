6a59a2bc905b48e1e50b41f9ee67eead60ed2ca8
Freeszeguet <56933699+Freeszeguet@users.noreply.github.com>
2024-08-19 14:23:43 -0300

    fiap_techchallenge_5.py

--- fiap_techchallenge_5.py
+++ fiap_techchallenge_5.py
@@ -3,54 +3,53 @@ import pandas as pd
 import pickle
 import numpy as np
 import plotly.graph_objs as go
-from PIL import Image  # Import Image module from PIL (Pillow)
+from PIL import Image  
 
-# Load the DataFrame and Model
+
 df_validacao = pd.read_csv('df_validacao.csv')
 with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
 
-# Load the vectorizer used during training
+
 with open('vectorizer.pkl', 'rb') as file:
     vectorizer = pickle.load(file)
 
-# Custom function to add symbols in "DIFERENÇA" column
+
 def add_custom_symbols(df):
     df['DIFERENCA'] = df['DIFERENCA'].apply(lambda x: '✓' if x else 'X')
     return df
 
-# Apply custom symbols to the "DIFERENÇA" column
+
 df_validacao = add_custom_symbols(df_validacao)
 
-# App Layout
+
 st.title("Análise de Ponto de Virada")
 
-# Top Section with Name, Course, and Registration
+
 st.sidebar.header("Informações do Estudante")
 st.sidebar.text("Nome: Gabriel Hagle Freesz")
-st.sidebar.text("Curso: [Nome do Curso]")
+st.sidebar.text("Curso: Data Analytics")
 st.sidebar.text("Matrícula: rm351357")
 
 
-# Title of the Project
+
 st.header("Projeto de Predição: Ponto de Virada")
 
-# Navigation Buttons
+
 page = st.sidebar.selectbox("Escolha a Análise", ["Análise Exploratória", "Modelo de Predição"])
 
 if page == "Análise Exploratória":
     st.subheader("Análise Exploratória")
-    # Insert your plotting code here
 
-    # Load and display images from a directory
-    image_dir = "plots/"  # Update this path to your directory
-    image_files = ["idade.png", "instituicao_ensino.png", "output.png","pedra.png","tempo_na)ong.png","turmas.png"]  # List of your image files
+
+    image_dir = "plots/" 
+    image_files = ["idade.png", "instituicao_ensino.png", "output.png","pedra.png","tempo_na)ong.png","turmas.png"]
     images = [
         {
             "filename": "idade.png",
             "title": "Gráfico de Distribuição de Idade",
             "caption": "Idade x Ano",
-            "description": "Este gráfico mostra a distribuição das idades dos alunos ao longo dos anos 2020, 2021 e 2022. A maioria dos alunos tem entre 9 e 14 anos, com picos notáveis em 10 anos de idade, indicando que essa faixa etária é a mais representativa entre os alunos da instituição. A distribuição é semelhante nos três anos, com uma leve queda em 2021."
+            "description": "Este gráfico mostra a distribuição das idades dos alunos ao longo dos anos 2020, 2021 e 2022. A maioria dos alunos tem entre 9 e 14 anos, com picos notáveis em 10 anos de idade, indicando que essa faixa etária é a mais representativa entre os alunos da instituição. Não existem dados para 2021 e 2022."
         },
         {
             "filename": "instituicao_ensino.png",
@@ -91,20 +90,17 @@ if page == "Análise Exploratória":
         st.image(image, caption=img["caption"], use_column_width=True)
         st.markdown(f"### {img['title']}")
         st.markdown(img["description"])
-        st.markdown("---")  # Separator line
+        st.markdown("---") 
 
 elif page == "Modelo de Predição":
     st.subheader("Modelo de Predição")
 
-    # Input Fields for Prediction
     destaque_ieg = st.text_area("Destaque IEG")
     destaque_ida = st.text_area("Destaque IDA")
     destaque_ipv = st.text_area("Destaque IPV")
     inde = st.number_input("INDE", min_value=0.0, max_value=10.0, step=0.01)
 
-    # Analyze Button
     if st.button("Analisar"):
-        # Process the inputs and make prediction
         input_data = {
             'DESTAQUE_IEG': [destaque_ieg], 
             'DESTAQUE_IDA': [destaque_ida], 
@@ -113,14 +109,11 @@ elif page == "Modelo de Predição":
         }
         input_df = pd.DataFrame(input_data)
 
-        # Vectorize the text fields
         input_text_combined = input_df['DESTAQUE_IEG'] + " " + input_df['DESTAQUE_IDA'] + " " + input_df['DESTAQUE_IPV']
         input_text_vectorized = vectorizer.transform(input_text_combined)
 
-        # Combine vectorized text data with numeric data
         input_final = pd.concat([pd.DataFrame(input_text_vectorized.toarray()), input_df[['INDE']].reset_index(drop=True)], axis=1)
 
-        # Make prediction using the model
         prediction = model.predict(input_final)
 
         if prediction[0] == 1:
@@ -128,7 +121,6 @@ elif page == "Modelo de Predição":
         else:
             st.error("Não atingiu o Ponto de Virada")
 
-        # Plot the effect of INDE on prediction using Plotly
         inde_values = np.linspace(0, 10, 100)
         predictions = []
 
@@ -138,7 +130,6 @@ elif page == "Modelo de Predição":
             pred = model.predict(input_final)
             predictions.append(pred[0])
 
-        # Apply smoothing
         def smooth_predictions(inde_values, predictions):
             smoothed_preds = []
             flag = False
@@ -153,7 +144,6 @@ elif page == "Modelo de Predição":
 
         smoothed_predictions = smooth_predictions(inde_values, predictions)
 
-        # Create a plotly figure
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=inde_values, y=smoothed_predictions, mode='lines+markers',
                                  marker=dict(color='blue'), name='Previsão'))
@@ -165,13 +155,10 @@ elif page == "Modelo de Predição":
             hovermode='x unified'
         )
 
-        # Display the plot
         st.plotly_chart(fig)
 
-    # Display the validation DataFrame with filtering options
     st.subheader("Casos Analisados")
     
-    # Add filtering options
     col1, col2, col3, col4, col5 = st.columns(5)
 
     with col1:
@@ -185,7 +172,6 @@ elif page == "Modelo de Predição":
     with col5:
         filtro_diferenca = st.selectbox("Filtrar por Diferença", options=["", "✓", "X"])
     
-    # Apply the filters
     if filtro_nome:
         df_validacao = df_validacao[df_validacao['NOME'] == filtro_nome]
     if filtro_inde:
@@ -197,10 +183,8 @@ elif page == "Modelo de Predição":
     if filtro_diferenca:
         df_validacao = df_validacao[df_validacao['DIFERENCA'] == filtro_diferenca]
 
-    # Show the DataFrame
     st.dataframe(df_validacao)
 
-# Set Layout Style
 st.markdown(
     """
     <style>
